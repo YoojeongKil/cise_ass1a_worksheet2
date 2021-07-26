732794b9e29da26e7b1256177148af7595d09b57
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2021-07-27 00:17:17 +1200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const articles = require("./dummydata/articles");
+const server = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+server.get('/api/dummydata/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+});
+
+server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
